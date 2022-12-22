d898c687efdd2b1fd776e13adada1a6072d617c8
Roman Tarnakin <roman_tarnakin@epam.com>
2022-12-22 20:13:02 +0300

    finish initial project setup

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+import classes from './Card.module.css';
+
+function Card(props) {
+  const { children } = props;
+
+  return <div className={classes.card}>{children}</div>;
+}
+
+Card.propTypes = {
+  children: PropTypes.number.isRequired,
+};
+
+export default Card;
